d1d57babf24cd3c96a2f1231cf281d28fe2fc8e7
Wonho Yoon <129478826+UnoYoon@users.noreply.github.com>
2025-07-29 18:58:48 +0900

    Time: 19 ms (90.05%), Space: 39.4 MB (28.58%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,15 @@
+# 연결리스트의 팰린드롬수 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        result = []
+        node = head
+        while node:
+            result.append(node.val)
+            node = node.next
+        return result == result[::-1]
