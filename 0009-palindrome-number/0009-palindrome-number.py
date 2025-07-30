aba03feca6a99504de3b4007f90b8dea5513c1b4
Wonho Yoon <129478826+UnoYoon@users.noreply.github.com>
2025-07-30 14:39:06 +0900

    Time: 2 ms (90.79%), Space: 17.9 MB (19.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,15 +3,14 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 :
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        if x == 0:
-            return True
         
-        reverse_x = ''.join(reversed(str(x)))
-        n = str(x)
-
-        if reverse_x == n :
-            return True
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
         
-        return False
+        # 언제 끝나냐? x = 12 , reversed_half = 123
+        
+        return x == reversed_half or x == reversed_half // 10
